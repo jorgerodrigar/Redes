d50fd3e33bbb06b460e600a016b147e8104645e5
Jorge Rodríguez García <jorgerodrigar2703@gmail.com>
2019-05-16 15:29:15 +0200

    empezada practica 6

---
+++ Practica6/Parte 1/ejercicio1.cc
@@ -0,0 +1,85 @@
+#include "Serializable.h"
+
+#include <iostream>
+#include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+class Jugador: public Serializable
+{
+public:
+    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    {
+        strncpy(name, _n, 80);
+    };
+
+    virtual ~Jugador(){};
+
+    void to_bin()
+    {
+        int fileIndex = open("ONE_W", O_RDWR | O_CREAT);
+
+        alloc_data(MAX_NAME*sizeof(char) + 2 * sizeof(int16_t));
+        char * tmp = _data;
+        memcpy(tmp, name, sizeof(char) * MAX_NAME);
+        write(fileIndex, tmp, sizeof(char) * MAX_NAME);
+        tmp += sizeof(char) * MAX_NAME; //se adelanta 
+        memcpy(tmp, &x, sizeof(int16_t));
+        write(fileIndex, tmp, sizeof(int16_t));
+        tmp += sizeof(int16_t); //se adelanta 
+        memcpy(tmp, &y, sizeof(int16_t));
+        write(fileIndex, tmp, sizeof(int16_t));
+
+        close(fileIndex);
+    }
+
+    int from_bin(char * fileName)
+    {
+        int fileIndex = open(fileName, O_RDWR);
+
+        char * tmp;
+        
+        read(fileIndex, tmp, sizeof(char) * MAX_NAME);
+        memcpy(name, tmp, sizeof(char) * MAX_NAME);
+        read(fileIndex, tmp, sizeof(int16_t));
+        memcpy(&x, tmp, sizeof(int16_t));
+        read(fileIndex, tmp, sizeof(int16_t));
+        memcpy(&y, tmp, sizeof(int16_t));
+
+        close(fileIndex);
+
+        return 0;
+    }
+
+    void dumpInfo(){
+        std::cout << name << " " << x << " " << y << std::endl;
+    }
+
+private:
+    static const size_t MAX_NAME = 80;
+    char name[MAX_NAME];
+
+    int16_t x;
+    int16_t y;
+};
+
+int main(int argc, char **argv)
+{
+    Jugador one_r("", 0, 0);
+    Jugador one_w("Player_ONE", 123, 987);
+
+    one_w.to_bin();
+    one_r.from_bin((char*)"ONE_W");
+    one_r.dumpInfo();
+
+
+    //Serializar y escribir one_w en un fichero
+    //Leer el fichero en un buffer y "deserializar" en one_r
+    //Mostrar el contenido one_r
+
+    return 0;
+}
---
+++ Practica6/Parte 2/Socket.cc
@@ -0,0 +1,94 @@
+#include <string.h>
+
+#include "Serializable.h"
+#include "Socket.h"
+
+Socket::Socket(const char * address, const char * port):sd(-1)
+{
+    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
+    //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    struct addrinfo hints;
+    memset((void*)&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    struct addrinfo *res;
+
+    int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+
+    if(rc != 0){
+        std::cout << "Error getaddrinfo: " << gai_strerror(rc) << std::endl;
+        return -1;
+    }
+
+	// protocolo = 0 -> el protocolo se elige en funcion del tipo de socket
+    sd = socket(res->ai_family, res->ai_socktype, 0);
+    sa =  res->ai_addr; 
+    sa_len = res->ai_addrlen;
+    bind();
+    freeaddrinfo(res);
+}
+
+int Socket::recv(Serializable &obj, Socket * &sock)
+{
+    struct sockaddr sa;
+    socklen_t sa_len = sizeof(struct sockaddr);
+
+    char buffer[MAX_MESSAGE_SIZE];
+
+    ssize_t bytes = ::recvfrom(sd, buffer, MAX_MESSAGE_SIZE, 0, &sa, &sa_len);
+
+    if ( bytes <= 0 )
+    {
+        return -1;
+    }
+
+    if ( sock != 0 )
+    {
+        sock = new Socket(&sa, sa_len);
+    }
+
+    obj.from_bin(buffer);
+
+    return 0;
+}
+
+int Socket::send(Serializable& obj, const Socket& sock)
+{
+    struct sockaddr sa;
+    socklen_t sa_len = sizeof(struct sockaddr);
+
+    char buffer[MAX_MESSAGE_SIZE];
+
+    obj.to_bin(buffer);
+
+    ssize_t bytes = sendto(sd, buffer, sizeof(buffer), 0, &sa, sa_len);
+
+    if ( bytes <= 0 )
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+bool operator== (const Socket &s1, const Socket &s2)
+{
+    //Comparar los campos sin_family, sin_addr.s_addr y sin_port
+    //de la estructura sockaddr_in de los Sockets s1 y s2
+    //Retornar false si alguno difiere
+    return (s1.sin_family == s2.sin_family && s1.sin_addr.s_addr == s2.sin_addr.s_addr && s1.sin_port == s2.sin_port);
+};
+
+std::ostream& operator<<(std::ostream& os, const Socket& s)
+{
+    char host[NI_MAXHOST];
+    char serv[NI_MAXSERV];
+
+    getnameinfo((struct sockaddr *) &(s.sa), s.sa_len, host, NI_MAXHOST, serv,
+                NI_MAXSERV, NI_NUMERICHOST);
+
+    os << host << ":" << serv;
+
+    return os;
+};
+
